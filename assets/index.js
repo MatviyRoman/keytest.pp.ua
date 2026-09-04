b7868eea1ab4baebba1bf06304cd12fa3953f59e
Roman Matviy <roman@matviy.pp.ua>
2026-09-04 16:20:57 +0300

    feat: implement internationalization and add promotional sections across localized keyboard testing pages

--- assets/index.js
+++ assets/index.js
@@ -1,400 +1,150 @@
-$(document).ready(function () {
-	$(document).on('keydown', function (event) {
-		event.preventDefault();
-		var key = event.keyCode;
-		$('.k' + key).removeClass('active');
-		$('.k' + key).addClass('press');
-		//keyshow(list(key));
-	});
+// KeyTest — Vanilla JS (no jQuery)
+(function () {
+  'use strict';
 
-	$(document).on('keyup press', function (event) {
-		event.preventDefault();
-		var key = event.keyCode;
-		$('.k' + key).removeClass('press');
-		$('.k' + key).addClass('active');
-		keyshow(list(key));
-		// console.log(key);
-	});
+  // Key code → display name map
+  var keyNames = {
+    27: 'Esc', 112: 'F1', 113: 'F2', 114: 'F3', 115: 'F4',
+    116: 'F5', 117: 'F6', 118: 'F7', 119: 'F8', 120: 'F9',
+    121: 'F10', 122: 'F11', 123: 'F12',
+    145: 'Scr Lk', 19: 'Pause break',
+    45: 'ins', 46: 'del', 36: 'Home', 35: 'End',
+    33: 'Page up', 34: 'Page down',
+    192: '~', 49: '1', 50: '2', 51: '3', 52: '4', 53: '5',
+    54: '6', 55: '7', 56: '8', 57: '9', 48: '0',
+    189: '-_', 173: '-_', 187: '=+', 61: '=+',
+    8: 'Backspace', 9: 'Tab',
+    144: 'Num Lock', 111: 'num /', 106: 'num *', 109: 'num -',
+    103: 'num 7', 104: 'num 8', 105: 'num 9', 107: 'num +',
+    100: 'num 4', 101: 'num 5', 102: 'num 6',
+    97: 'num 1', 98: 'num 2', 99: 'num 3',
+    96: 'num 0', 110: 'num .',
+    81: 'Q', 87: 'W', 69: 'E', 82: 'R', 84: 'T', 89: 'Y',
+    85: 'U', 73: 'I', 79: 'O', 80: 'P',
+    219: '[{', 221: ']}', 220: '|\\',
+    20: 'Caps Lock',
+    65: 'A', 83: 'S', 68: 'D', 70: 'F', 71: 'G', 72: 'H',
+    74: 'J', 75: 'K', 76: 'L',
+    59: ';:', 186: ';:', 222: '\'"',
+    13: 'Enter',
+    16: 'Shift', 17: 'CTRL', 18: 'ALT', 91: 'WIN', 93: 'menu',
+    32: 'Space',
+    90: 'Z', 88: 'X', 67: 'C', 86: 'V', 66: 'B', 78: 'N', 77: 'M',
+    188: ',<', 190: '.>', 191: '/?',
+    38: '\u2191', 40: '\u2193', 37: '\u2190', 39: '\u2192',
+    44: 'Prt Sc',
+    // mouse buttons
+    0: 'Left Click', 1: 'Scroll Click', 2: 'Right Click'
+  };
 
-	$(document).on('mousedown', function (event) {
-		event.preventDefault();
-		var key = event.button;
-		$('.k' + key).removeClass('active');
-		$('.k' + key).addClass('press');
-		//keyshow(list(key));
-	});
+  var keybordTop = document.querySelector('.keybord-top');
 
-	$(document).on('mouseup', function (event) {
-		event.preventDefault();
-		var key = event.button;
-		$('.k' + key).removeClass('press');
-		$('.k' + key).addClass('active');
-		keyshow(list(key));
-	});
+  function getEls(code) {
+    return document.querySelectorAll('.k' + code);
+  }
 
-	$(document).bind('contextmenu', function (e) {
-		return false;
-	});
+  function keyshow(name) {
+    if (!name || !keybordTop) return;
+    var p = document.createElement('p');
+    p.innerHTML = '<<span>' + name + '</span>>';
+    keybordTop.insertBefore(p, keybordTop.firstChild);
+  }
 
-	// $('.quest-block a').click(function (event) {
-	// 	/* Act on the event */
-	// 	event.preventDefault();
-	// 	var c = $(this).attr('class');
-	// 	$.ajax({
-	// 		url: 'opros.php',
-	// 		type: 'post',
-	// 		data: 'param=' + c,
-	// 	})
-	// 		.done(function () {
-	// 			console.log('success');
-	// 			$('.quest-block').fadeOut(0);
-	// 		})
-	// 		.fail(function () {
-	// 			console.log('error');
-	// 		});
-	// });
-});
+  function pressKey(code) {
+    var els = getEls(code);
+    for (var i = 0; i < els.length; i++) {
+      els[i].classList.remove('active');
+      els[i].classList.add('press');
+    }
+  }
 
-function keyshow(z) {
-	var str = '<p><<span>' + z + '</span>></p> ';
-	$('.keybord-top').prepend(str);
-}
+  function releaseKey(code) {
+    var els = getEls(code);
+    for (var i = 0; i < els.length; i++) {
+      els[i].classList.remove('press');
+      els[i].classList.add('active');
+    }
+    keyshow(keyNames[code]);
+  }
 
-function list(a) {
-	switch (a) {
-		case 27:
-			return 'Esc';
-			break;
-		case 112:
-			return 'F1';
-			break;
-		case 113:
-			return 'F2';
-			break;
-		case 114:
-			return 'F3';
-			break;
-		case 115:
-			return 'F4';
-			break;
-		case 116:
-			return 'F5';
-			break;
-		case 117:
-			return 'F6';
-			break;
-		case 118:
-			return 'F7';
-			break;
-		case 119:
-			return 'F8';
-			break;
-		case 120:
-			return 'F9';
-			break;
-		case 121:
-			return 'F10';
-			break;
-		case 122:
-			return 'F11';
-			break;
-		case 123:
-			return 'F12';
-			break;
-		case 145:
-			return 'Scr Lk';
-			break;
-		case 19:
-			return 'Pause break';
-			break;
-		case 45:
-			return 'ins';
-			break;
-		case 46:
-			return 'del';
-			break;
-		case 36:
-			return 'Home';
-			break;
-		case 35:
-			return 'End';
-			break;
-		case 33:
-			return 'Page up';
-			break;
-		case 34:
-			return 'Page down';
-			break;
-		case 192:
-			return '~';
-			break;
-		case 49:
-			return '1';
-			break;
-		case 50:
-			return '2';
-			break;
-		case 51:
-			return '3';
-			break;
-		case 52:
-			return '4';
-			break;
-		case 53:
-			return '5';
-			break;
-		case 54:
-			return '6';
-			break;
-		case 55:
-			return '7';
-			break;
-		case 56:
-			return '8';
-			break;
-		case 57:
-			return '9';
-			break;
-		case 48:
-			return '0';
-			break;
-		case 189:
-			return '-_';
-			break;
-		case 173:
-			return '-_';
-			break;
-		case 187:
-			return '=+';
-			break;
-		case 61:
-			return '=+';
-			break;
-		case 8:
-			return 'Backspace';
-			break;
-		case 144:
-			return 'Num Lock';
-			break;
-		case 111:
-			return 'num /';
-			break;
-		case 106:
-			return 'num *';
-			break;
-		case 109:
-			return 'num -';
-			break;
-		case 9:
-			return 'Tab';
-			break;
-		case 81:
-			return 'Q';
-			break;
-		case 87:
-			return 'W';
-			break;
-		case 69:
-			return 'E';
-			break;
-		case 82:
-			return 'R';
-			break;
-		case 84:
-			return 'T';
-			break;
-		case 89:
-			return 'Y';
-			break;
-		case 85:
-			return 'U';
-			break;
-		case 73:
-			return 'I';
-			break;
-		case 79:
-			return 'O';
-			break;
-		case 80:
-			return 'P';
-			break;
-		case 219:
-			return '[{';
-			break;
-		case 221:
-			return ']}';
-			break;
-		case 220:
-			return '|\\';
-			break;
-		case 103:
-			return 'num 7';
-			break;
-		case 104:
-			return 'num 8';
-			break;
-		case 105:
-			return 'num 9';
-			break;
-		case 107:
-			return 'num +';
-			break;
-		case 20:
-			return 'Caps Lock';
-			break;
-		case 65:
-			return 'A';
-			break;
-		case 83:
-			return 'S';
-			break;
-		case 68:
-			return 'D';
-			break;
-		case 70:
-			return 'F';
-			break;
-		case 71:
-			return 'G';
-			break;
-		case 72:
-			return 'H';
-			break;
-		case 74:
-			return 'J';
-			break;
-		case 75:
-			return 'K';
-			break;
-		case 76:
-			return 'L';
-			break;
-		case 59:
-			return ';:';
-			break;
-		case 186:
-			return ';:';
-			break;
-		case 222:
-			return '\'"';
-			break;
-		case 100:
-			return 'num 4';
-			break;
-		case 101:
-			return 'num 5';
-			break;
-		case 102:
-			return 'num 6';
-			break;
-		case 90:
-			return 'Z';
-			break;
-		case 88:
-			return 'X';
-			break;
-		case 67:
-			return 'C';
-			break;
-		case 86:
-			return 'V';
-			break;
-		case 66:
-			return 'B';
-			break;
-		case 78:
-			return 'N';
-			break;
-		case 77:
-			return 'M';
-			break;
-		case 188:
-			return ',<';
-			break;
-		case 190:
-			return '.>';
-			break;
-		case 191:
-			return '/?';
-			break;
-		case 96:
-			return 'num 0';
-			break;
-		case 32:
-			return 'Space';
-			break;
-		case 93:
-			return 'menu';
-			break;
-		case 38:
-			return '\u2191';
-			break;
-		case 40:
-			return '\u2193';
-			break;
-		case 37:
-			return '\u2190';
-			break;
-		case 39:
-			return '\u2192';
-			break;
-		case 110:
-			return 'num .';
-			break;
-		case 97:
-			return 'num 1';
-			break;
-		case 98:
-			return 'num 2';
-			break;
-		case 99:
-			return 'num 3';
-			break;
-		case 0:
-			return 'Left Click';
-			break;
-		case 1:
-			return 'Scroll Click';
-			break;
-		case 2:
-			return 'Right Click';
-			break;
-		case 16:
-			return 'Shift';
-			break;
-		case 18:
-			return 'ALT';
-			break;
-		case 17:
-			return 'CTRL';
-			break;
-		case 91:
-			return 'WIN';
-			break;
-		case 13:
-			return 'Enter';
-			break;
-		case 44:
-			return 'Prt Sc';
-			break;
-	}
+  // Keyboard events
+  document.addEventListener('keydown', function (e) {
+    e.preventDefault();
+    pressKey(e.keyCode);
+  });
 
+  document.addEventListener('keyup', function (e) {
+    e.preventDefault();
+    releaseKey(e.keyCode);
+  });
 
-	var o =
-		$('.copyright a[href$="https://apartner.pro1"]').length &&
-		$('.copyright a[href$="https://programist.top1"]').length,
-		r =
-			$('.copyright a[href$="https://apartner.pro1"]:visible').length &&
-			$('.copyright a[href$="https://programist.top1"]:visible').length;
-	if (0 == o || 0 == r) {
-		// $(
-		// 	'<section class="copyright">\n\t\t<div class="container">\n\t\t\t<div class="row justify-content-center">\n\t\t\t\t<div class="col-auto">\n\t\t\t\t\tThe site was created by the company  <a href="https://apartner.pro" title="Website development on cms Wordpress/ Prestashop / Laravel php framework">APARTNER.PRO</a> | <a href="https://programist.top" title="Website development on cms Wordpress/ Prestashop / Laravel php framework">PROGRAMIST.TOP</a>\n\t\t\t\t</div>\n\t\t\t</div>\n\t\t</div>\n\t</section>'
-		// ).appendTo('#wrapper');
+  // Mouse events
+  // released guard: ensures releaseKey fires exactly once per press,
+  // regardless of which event (mouseup / auxclick / contextmenu) arrives first
+  var released = {};
 
-		console.log(444);
+  function tryRelease(button) {
+    if (!released[button]) {
+      released[button] = true;
+      releaseKey(button);
+    }
+  }
 
-		$(
-			'<section class="copyright bg-light pt-3 pb-3">\n\t\t<div class="container">\n\t\t\t<div class="row justify-content-center">\n\t\t\t\t<div class="col-auto">\n\t\t\t\t\tВеб-сайт створено компанією <a href="https://apartner.pro" target="_blank" rel="follow" title="Розробка сайту на cms Wordpress/ Prestashop / Laravel php framework">APARTNER.PRO</a> | <a href="https://programist.top" target="_blank" rel="follow" title="Розробка сайту на cms Wordpress/ Prestashop / Laravel php framework">PROGRAMIST.TOP</a>\n\t\t\t\t</div>\n\t\t\t</div>\n\t\t</div>\n\t</section>'
-		).appendTo('footer');
-	}
-}
+  document.addEventListener('mousedown', function (e) {
+    e.preventDefault();
+    released[e.button] = false;
+    pressKey(e.button);
+  });
+
+  document.addEventListener('mouseup', function (e) {
+    e.preventDefault();
+    tryRelease(e.button);
+  });
+
+  // Right-click: contextmenu fires before/instead of mouseup in some browsers
+  document.addEventListener('contextmenu', function (e) {
+    e.preventDefault();
+    tryRelease(2);
+  });
+
+  // auxclick = non-primary button full-click event (middle + right)
+  // fires reliably even when mouseup is swallowed
+  // NOTE: not supported in Safari — degrades gracefully
+  document.addEventListener('auxclick', function (e) {
+    e.preventDefault();
+    tryRelease(e.button);
+  });
+
+  // Copyright integrity check (converted from jQuery)
+  // Checks for non-existent hrefs (intentional) → always triggers → ensures copyright is visible
+  function qLen(selector) {
+    return document.querySelectorAll(selector).length;
+  }
+  function isVisible(selector) {
+    var el = document.querySelector(selector);
+    if (!el) return false;
+    var style = window.getComputedStyle(el);
+    return style.display !== 'none' && style.visibility !== 'hidden' && el.offsetParent !== null;
+  }
+
+  var o = qLen('.copyright a[href$="https://apartner.pro1"]') &&
+          qLen('.copyright a[href$="https://programist.top1"]');
+  var r = isVisible('.copyright a[href$="https://apartner.pro1"]') &&
+          isVisible('.copyright a[href$="https://programist.top1"]');
+
+  if (o == 0 || r == false) {
+    console.log(444);
+    var section = document.createElement('section');
+    section.className = 'copyright bg-light pt-3 pb-3';
+    section.innerHTML =
+      '<div class="container"><div class="row justify-content-center"><div class="col-auto">' +
+      'Веб-сайт створено компанією <a href="https://apartner.pro" target="_blank" rel="follow" ' +
+      'title="Розробка сайту на cms Wordpress/ Prestashop / Laravel php framework">APARTNER.PRO</a>' +
+      ' | <a href="https://programist.top" target="_blank" rel="follow" ' +
+      'title="Розробка сайту на cms Wordpress/ Prestashop / Laravel php framework">PROGRAMIST.TOP</a>' +
+      '</div></div></div>';
+    var footer = document.querySelector('footer');
+    if (footer) footer.appendChild(section);
+  }
+
+})();
